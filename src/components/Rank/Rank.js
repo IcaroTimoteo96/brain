655f5ff323a82d3db4f321688d57a63d91714543
IcaroTimoteo96 <46626881+IcaroTimoteo96@users.noreply.github.com>
2020-12-19 19:51:02 -0300

    upload my first front end

---
+++ src/components/Rank/Rank.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Rank = () => {
+    return(
+      <div className = 'white f3'>
+          <div>
+                {'#5 this your rank'}
+          </div>
+      </div>
+    );
+
+}
+
+export default Rank;
