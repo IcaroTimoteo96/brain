c3d151f40b8557fc3528bf51146a34691fdadd9f
IcaroTimoteo96 <46626881+IcaroTimoteo96@users.noreply.github.com>
2021-01-18 22:30:08 -0300

    update to heroku

--- src/App.js
+++ src/App.js
@@ -94,7 +94,7 @@ class App extends Component {
       console.log('hi', response)
       if(response){
         
-        fetch('http://localhost:3000/image', {
+        fetch('https://quiet-ridge-77223.herokuapp.com/image', {
           method: 'put',
           headers: {'Content-Type': 'application/json'},
           body: JSON.stringify({
